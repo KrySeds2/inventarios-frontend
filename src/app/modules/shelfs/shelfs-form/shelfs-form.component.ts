49d1551149384d22cfdc14597c68609ecfc5e543
Edson Blanco <lotad_lombre@hotmail.com>
2023-05-04 12:13:33 -0600

    feat:creacion del modulo shelfs y componentes add,edit, form y shelfs

---
+++ src/app/modules/shelfs/shelfs-form/shelfs-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shelfs-form',
+  templateUrl: './shelfs-form.component.html',
+  styleUrls: ['./shelfs-form.component.scss']
+})
+export class ShelfsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/shelfs/shelfs.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ShelfsComponent } from './view/shelfs.component';
+import { ModuleRoutingModule } from '../module-routing.module';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ShelfsFormComponent } from './shelfs-form/shelfs-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    ShelfsComponent,
+    AddComponent,
+    EditComponent,
+    ShelfsFormComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ModuleRoutingModule,
+
+  ]
+})
+export class ShelfsModule { }
