8f0e00fe3a0c72c069ea1639ec94bf84072a645f
Edson Blanco <lotad_lombre@hotmail.com>
2023-05-04 12:30:03 -0600

    fix:modificacion en importaciones del modulo shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { FormsModule } from '@angular/forms';
+// Modulos
 import { TableCustomModule } from './modules/tables/table-custom.module';
 import { DirectivesModule } from './modules/directives/directives.module';
 import { PrimengImportModule } from './modules/primeng-import/primeng-import.module';
 import { ButtonsModule } from './modules/buttons/buttons.module';
-
+import { ComponentsModule } from './components/components.module';
 @NgModule({
   declarations: [
 
@@ -19,18 +16,16 @@ import { ButtonsModule } from './modules/buttons/buttons.module';
     FormsModule,
     DirectivesModule,
     PrimengImportModule,
-    ButtonsModule
+    ButtonsModule,
+    ComponentsModule
 
   ], exports: [
-    MatSidenavModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule,
     FormsModule,
     TableCustomModule,
     DirectivesModule,
     PrimengImportModule,
-    ButtonsModule
+    ButtonsModule,
+    ComponentsModule
   ]
 })
 export class SharedModule { }
