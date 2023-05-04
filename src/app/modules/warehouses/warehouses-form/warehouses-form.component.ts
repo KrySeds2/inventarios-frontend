5e64388082edaf2077ebe71cad325ab69e11db48
Edson Blanco <lotad_lombre@hotmail.com>
2023-05-04 12:23:10 -0600

    feat:creacion del modulo warehouses y componentes add,edit y form

---
+++ src/app/modules/warehouses/warehouses-form/warehouses-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouses-form',
+  templateUrl: './warehouses-form.component.html',
+  styleUrls: ['./warehouses-form.component.scss']
+})
+export class WarehousesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
