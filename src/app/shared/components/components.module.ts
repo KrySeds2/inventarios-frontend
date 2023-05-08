c7d62e744582201e42de844a78f00d8fc368acb3
Edson Blanco <lotad_lombre@hotmail.com>
2023-05-08 17:49:42 -0600

    feat:add new imports in components module

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -4,19 +4,19 @@ import { CommonModule } from '@angular/common';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { TitleComponent } from './title/title.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { PrimengImportModule } from '../modules/primeng-import/primeng-import.module';
 
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 @NgModule({
   declarations: [
     MainContainerComponent,
     TitleComponent,
     SidenavComponent,
-    ToolbarComponent
+    BreadcrumbsComponent,
   ],
   imports: [
     CommonModule,
@@ -30,7 +30,6 @@ import { MatListModule } from '@angular/material/list';
     MainContainerComponent,
     TitleComponent,
     SidenavComponent,
-    ToolbarComponent
   ]
 })
 export class ComponentsModule { }
