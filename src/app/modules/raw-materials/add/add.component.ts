2bd78aa9baeb1ba9358dea62570dd3ad9050024f
Edson Blanco <lotad_lombre@hotmail.com>
2023-05-09 17:56:53 -0600

    feat:create forms of raw materials

--- src/app/modules/raw-materials/add/add.component.ts
+++ src/app/modules/raw-materials/add/add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RawMaterialsTransformService } from '../services/raw-materials-transform.service';
+import { RawMaterialsCrudService } from 'src/app/shared/services/raw-materials/raw-materials-crud.service';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+  formData: FormGroup;
+  isDisabled: boolean=false;
+  constructor(
+    // private jwtService: JwtService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private rawMaterialsTransformService: RawMaterialsTransformService,
+    private rawMaterialsCrudService: RawMaterialsCrudService
+  ) {
+    this.declareForm();
+  }
 
-  constructor() { }
+  declareForm():void{
+    this.formData = this.fb.group({
+      name:[,[Validators.required]],
+      idUnicoEscanear:[,[Validators.required]],
+      descripcion:[]
+    })
+  }
 
   ngOnInit(): void {
+  }
+  submit(){
+
+  }
+
+  back(): void {
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
 }
--- src/app/modules/raw-materials/view/raw-materials.component.ts
+++ src/app/modules/raw-materials/view/raw-materials.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TableHead } from 'src/app/shared/modules/tables/models/tableHead';
 import { RawMaterialsRow } from '../models/raw-materials-row';
+import { LoadingComponent } from 'src/app/shared/modules/dialogs/components/loading/loading.component';
+import { DialogErrorComponent } from 'src/app/shared/modules/dialogs/components/dialog-error/dialog-error.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { RawMaterialsCrudService } from 'src/app/shared/services/raw-materials/raw-materials-crud.service';
 
 @Component({
   selector: 'app-raw-materials',
@@ -8,6 +13,8 @@ import { RawMaterialsRow } from '../models/raw-materials-row';
   styleUrls: ['./raw-materials.component.scss']
 })
 export class RawMaterialsComponent implements OnInit {
+  @ViewChild('dialogLoading') loadingComponent: LoadingComponent;
+  @ViewChild('dialogError') dialogError: DialogErrorComponent;
   columnsTable: TableHead<RawMaterialsRow>[] = [
     { header: 'N°', field: 'index', width: '20px', maxWidth: '50px', align: 'center', custom: false },
     { header: 'Nombre', field: 'name', width: '100px', align: 'center' },
@@ -19,30 +26,41 @@ export class RawMaterialsComponent implements OnInit {
   ];
   listItems: RawMaterialsRow[] = [{
     index: 1,
+    id:'12121',
     name: 'MTP-01',
     idUnicoEscanear: '45896-0324-5632',
     status: true,
   }, {
     index: 2,
+    id:'2365',
     name: 'MTP-02',
     idUnicoEscanear: '9653-55454-4512',
     status: true,
   }, {
     index: 3,
+    id:'5678',
     name: 'MTP-03',
     idUnicoEscanear: '6524-5631-56544',
     status: false,
   }, {
     index: 4,
+    id:'98765',
     name: 'MTP-04',
     idUnicoEscanear: '63963-14523-4563',
     status: true,
   }];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private translate: TranslateService,
+    private rawMaterialsCrudService: RawMaterialsCrudService
+  ) { }
 
   ngOnInit(): void {
     this.listItems;
   }
-
+  selectedEditConfirm(item: RawMaterialsRow): void {
+    this.router.navigate(['./edit', item?.id], { relativeTo: this.route });
+  }
 }
