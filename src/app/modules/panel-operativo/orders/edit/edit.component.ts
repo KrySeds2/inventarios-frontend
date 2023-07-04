bde1247992f6addc5d2c6793ad72ce0ad39ef787
Edson Blanco <lotad_lombre@hotmail.com>
2023-07-04 09:37:29 -0600

    feat:terminar estructura del modulo de orders

--- src/app/modules/panel-operativo/orders/edit/edit.component.ts
+++ src/app/modules/panel-operativo/orders/edit/edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OrdersTransformService } from '../services/orders-transform.service';
+import { OrdersCrudService } from '@shared/services/orders/orders-crud.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  formData: FormGroup;
+  isDisabled: boolean=false;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private ordersTransformService: OrdersTransformService,
+    private ordersCrudService: OrdersCrudService
+  ) {
+    this.declareForm();
+   }
 
-  constructor() { }
-
+  declareForm():void{
+    this.formData = this.fb.group({
+      folio:[,[Validators.required]],
+      producto:[,[Validators.required]],
+      cantidad:[,[Validators.required]],
+    })
+  }
   ngOnInit(): void {
   }
 
+  submit(){
+
+  }
 }
