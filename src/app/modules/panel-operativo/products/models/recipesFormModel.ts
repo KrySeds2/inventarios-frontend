62104560290f7c571a9eb3bc3a73f29859201655
Edson Blanco <lotad_lombre@hotmail.com>
2023-07-27 17:54:44 -0600

    feat:creacion de form model de recipes

---
+++ src/app/modules/panel-operativo/products/models/recipesFormModel.ts
@@ -0,0 +1,11 @@
+
+import { RawMaterialsResponse } from "@shared/services/raw-materials/responses/rawMaterialsResponse";
+
+
+export interface RecipesFormModel {
+
+  materialId:RawMaterialsResponse[];
+  amount_to_use:number;
+
+
+}
